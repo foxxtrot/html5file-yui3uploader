c2ed1346c9c4a9db75c1377ef81bd180cf4f5712
Jeff Craig <foxxtrot@foxxtrot.net>
2011-03-31 17:50:24 -0700

    Extracting XHR Level 2 interface into it's own module

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,3 +1,40 @@
+YUI.add('io-file-xdr2', function(Y) {
+    var io = Y.io,
+        sub = Y.Lang.sub;
+
+    // Y.io is incredibly inflexible right now.
+
+    io.file = function(uri, c, i) {
+        var parts = [], f, fd,
+            xhr = w.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+        c = c || {};
+        i = i || io._id();
+
+        c.boundary = c.boundary || Y.guid();
+        c.method = 'POST';
+        c.headers = Y.merge(c.headers, { 'Content-Type': ['multipart/form-data; boundary=', c.boundary].join('')});
+        if (c.form) {
+            if (typeof(c.form) === 'string') {
+                f = Y.Selector.query(document, '#' + c.form, true)[0];
+            } else {
+                // Allow Y.Node or DOM Node, works more like existing io-form impl
+                f = c.form._node || c.form;
+            }
+            delete c.form;
+            fd = new FormData(f);
+        } else {
+            fd = new FormData();
+        }
+        Y.Array.each(c.files, function(file) {
+            fd.append('file', file);
+        });
+        c.data = fd;
+        return Y.io.http(uri, c, i);
+    };
+}, '1.0.0', { requires: ['io-base', 'io-form'], supersedes: 'io-file'});
+
+
 YUI.add('io-file', function(Y) {
     var io = Y.io,
         sub = Y.Lang.sub,
@@ -24,52 +61,75 @@ YUI.add('io-file', function(Y) {
             }
         };
     io.file = function(uri, c, i) {
-        var parts = [], f;
+        var parts = [], f, fd;
         c = c || {};
         i = i || io._id();
         c.boundary = c.boundary || Y.guid();
         c.method = 'POST';
-        if(c.form) {
-            f = Y.io._serialize(c.form, c.data).split('&');
-            Y.Array.each(f, function(rec) {
-                var key, value;
-                rec = rec.split('=');
-                key = rec[0]; value = rec[1];
-
-                if (value !== '') {
-                    parts.push(['Content-Disposition: form-data; name="' + key + '"', "", value].join('\r\n'));
+        c.headers = Y.merge(c.headers, { 'Content-Type': ['multipart/form-data; boundary=', c.boundary].join('')});
+        // TODO: Extract this into a method so that the implementation can be conditionally loaded
+        if(Y.config.win.FormData) {
+            if (c.form) {
+                if (typeof(c.form) === 'string') {
+                    f = Y.Selector.query(document, '#' + c.form, true)[0];
+                } else {
+                    // Allow Y.Node or DOM Node, works more like existing io-form impl
+                    f = c.form._node || c.form;
                 }
+                delete c.form;
+                fd = new FormData(f);
+            } else {
+                fd = new FormData();
+            }
+            Y.Array.each(c.files, function(file) {
+                fd.append('file', file);
             });
-            // I don't want io itself to process the form object
-            delete c.form;
-        }
-        data[i] = {
-            config: c,
-            parts: parts,
-            uri: uri,
-            id: i,
-            processed: 0
-        };
-        Y.Array.each(c.files, function(file) {
-            var reader = new FileReader();
+            c.data = fd;
+            return Y.io.http(uri, c, i);
+        } else {
+            if(c.form) {
+                f = Y.io._serialize(c.form, c.data).split('&');
+                Y.Array.each(f, function(rec) {
+                    var key, value;
+                    rec = rec.split('=');
+                    key = rec[0]; value = rec[1];
 
-            reader.onload = function(e) {
-                Y.log('FileSize: ' + file.size);
-                Y.log('Result Size:' + reader.result.length);
-                // Per RFC 2616 HTTP is not a MIME compliant protocol, whereas I was trying to
-                // upload as Base64 using the Content-Transfer-Encoding MIME Header, that is
-                // not supported. This sucks a bit.
-                parts.push([sub('Content-Disposition: form-data; name="files"; filename="{name}"', file),
-                   sub("Content-Type: {type}", file),
-                   "",
-                   reader.result].join('\r\n'));
-                data[i].processed += 1;
+                    if (value !== '') {
+                        parts.push(['Content-Disposition: form-data; name="' + key + '"', "", value].join('\r\n'));
+                    }
+                });
+                // I don't want io itself to process the form object
+                delete c.form;
+            }
+
+            data[i] = {
+                config: c,
+                parts: parts,
+                uri: uri,
+                id: i,
+                processed: 0
             };
-            reader.readAsBinaryString(file);
-        });
+            Y.Array.each(c.files, function(file) {
+                var reader = new FileReader();
 
-        if (!laterHandle) {
-            laterHandle = Y.later(500, Y, checkForUpload, null, true);
+                reader.onload = function(e) {
+                    Y.log('FileSize: ' + file.size);
+                    Y.log('Result Size:' + reader.result.length);
+                    // Per RFC 2616 HTTP is not a MIME compliant protocol, whereas I was trying to
+                    // upload as Base64 using the Content-Transfer-Encoding MIME Header, that is
+                    // not supported. This sucks a bit.
+                    parts.push([sub('Content-Disposition: form-data; name="files"; filename="{name}"', file),
+                       sub("Content-Type: {type}", file),
+                       "",
+                       reader.result].join('\r\n'));
+                    data[i].processed += 1;
+                };
+                reader.readAsBinaryString(file);
+            });
+
+            if (!laterHandle) {
+                laterHandle = Y.later(500, Y, checkForUpload, null, true);
+            }
         }
     };
 }, '1.0.0', { requires: ['io-base', 'io-form'] });
